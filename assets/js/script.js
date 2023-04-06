b2e0dbbaafbb175f6f093d15d8fcc7c14e716d58
Rafael Antunes <73602484+rafael-antunes@users.noreply.github.com>
2023-04-06 14:30:52 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+function Calculadora() {
+
+    this.inicia = () => {
+        this.capturaCliques();
+        this.capturaEnter();
+    };
+
+
+    this.display = document.querySelector('.display');
+
+    this.capturaCliques = () => {
+        document.addEventListener('click', event => {
+        const el = event.target;
+        if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+        if (el.classList.contains('btn-clear')) this.clear();
+        if (el.classList.contains('btn-del')) this.del();
+        if (el.classList.contains('btn-eq')) this.realizaConta();
+        this.display.focus();
+        });
+    };
+
+    this.addNumDisplay = (el) => this.display.value += el.innerText;
+
+    this.clear = () => this.display.value = '';
+
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
+    
+
+    this.realizaConta = () => {
+        try {
+            const conta = eval(this.display.value);
+
+            if(!conta) {
+                alert('Conta inválida');
+                return;
+            }
+
+            this.display.value = conta;
+
+        } catch(e) {
+            alert('Conta inválida');
+            return
+        }
+    };
+
+    this.capturaEnter = () => {
+        this.display.addEventListener('keypress', e => {
+            if (e.keyCode !== 13) return;
+            this.realizaConta();
+        });
+    }
+
+   
+}
+
+const calculadora = new Calculadora();
+calculadora.inicia();
